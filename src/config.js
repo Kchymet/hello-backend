3f2f2f00f6330e2063ff72f3f05c81f43e70829b
Kyle Chynoweth <kchymet@gmail.com>
2018-01-15 18:57:58 -0600

    configuration for sample app

---
+++ src/config.js
@@ -0,0 +1,4 @@
+
+module.exports = {
+    port: process.env.LOCAL_PORT
+};
--- src/server.js
+++ src/server.js
@@ -1,10 +1,18 @@
 
 var express = require('express');
+var config = require('./config');
 
 var app = express();
+var port = config.port;
+
+if(!port){
+    port = 8080;
+    log.warn('Port not defined - defaulting to ' + port);
+}
 
 app.get('/',function(req, res){
     res.json("javascript");
 });
 
-app.listen(80);
+console.log('binding to port: '+port);
+app.listen(port);
